be3d030262d31cee9f5656705ac6cfaee6813873
ITsverkunov <tsverkunovivan@gmail.com>
2022-06-21 17:12:33 +0300

    fix: Status code

--- controllers/users.js
+++ controllers/users.js
@@ -41,7 +41,7 @@ module.exports.updateProfile = (req, res) => {
   const { name } = req.body
   User.findByIdAndUpdate(req.user._id, { name },     {
     new: true, // обработчик then получит на вход обновлённую запись
-    // runValidators: true // данные будут валидированы перед изменением
+    runValidators: true // данные будут валидированы перед изменением
   })
     .then(user => res.status(OK).send({ user }))
     .catch(err => {
