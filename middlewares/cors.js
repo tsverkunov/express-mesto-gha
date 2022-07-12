272190e4d76e893888cd57a3566a6e17f27fbb49
ITsverkunov <tsverkunovivan@gmail.com>
2022-07-12 12:44:52 +0300

    fix: CORS midleware4

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -14,7 +14,7 @@ module.exports.cors = (req, res, next) => {
 
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const { requestHeaders } = req.headers['access-control-request-headers'];
+  const requestHeaders = req.headers['access-control-request-headers'];
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
