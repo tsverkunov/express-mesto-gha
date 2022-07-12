b65919b7a1c1ec10280bef6824a68393334ff27b
ITsverkunov <tsverkunovivan@gmail.com>
2022-07-12 11:31:02 +0300

    fix: CORS midleware

--- app.js
+++ app.js
@@ -29,19 +29,18 @@ const allowedCors = [
 
 app.use((req, res, next) => {
   const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-allow-origin'];
+
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
 
-  const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-  }
-
-  const requestHeaders = req.headers['access-control-allow-origin'];
-  if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
   }
 
   next();
