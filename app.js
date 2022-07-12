73bc0e5be1c64a705e9abaad0cdf4cc4b655485f
ITsverkunov <tsverkunovivan@gmail.com>
2022-07-12 14:25:45 +0300

    fix: test cookies

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const rateLimit = require('express-rate-limit');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
 const { celebrate, Joi, errors } = require('celebrate');
 const { login, createUser } = require('./controllers/users');
 const { auth } = require('./middlewares/auth');
@@ -24,6 +25,7 @@ app.use(cors)
 
 app.use(limiter);
 app.use(bodyParser.json());
+app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb')
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,8 @@ const { checkToken } = require('../utils/jwt');
 const EmailOrPasswordError = require('../errors/EmailOfPasswordError');
 
 module.exports.auth = (req, res, next) => {
-  const { authorization } = req.headers;
+  const { authorization } = req.cookie.jwt;
+  console.log(req.cookie.jwt)
   if (!authorization || !authorization.startsWith('Bearer ')) {
     throw new EmailOrPasswordError('Необходимо авторизоваться');
   }
@@ -18,3 +19,20 @@ module.exports.auth = (req, res, next) => {
 
   next();
 };
+// module.exports.auth = (req, res, next) => {
+//   const { authorization } = req.headers;
+//   if (!authorization || !authorization.startsWith('Bearer ')) {
+//     throw new EmailOrPasswordError('Необходимо авторизоваться');
+//   }
+//
+//   const token = authorization.replace('Bearer ', '');
+//   let payload;
+//   try {
+//     payload = checkToken(token);
+//   } catch (err) {
+//     next(new EmailOrPasswordError('Необходимо авторизоваться'));
+//   }
+//   req.user = payload;
+//
+//   next();
+// };
