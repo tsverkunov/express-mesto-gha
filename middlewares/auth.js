e5a4df948adb8f61c4977ed12fb19f0d692069dc
ITsverkunov <tsverkunovivan@gmail.com>
2022-07-13 10:59:25 +0300

    fix: token from cookies

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,10 +1,30 @@
 const { checkToken } = require('../utils/jwt');
 const EmailOrPasswordError = require('../errors/EmailOfPasswordError');
 
+module.exports.auth = (req, res, next) => {
+  const token = req.cookies.jwt;
+  console.log(req.cookies.jwt)
+  if (!token) {
+    throw new EmailOrPasswordError('Необходимо авторизоваться');
+  }
+
+  let payload;
+  try {
+    payload = checkToken(token);
+  } catch (err) {
+    next(new EmailOrPasswordError('Необходимо авторизоваться'));
+  }
+  req.user = payload;
+
+  next();
+};
+
+
 // module.exports.auth = (req, res, next) => {
-//   const { authorization } = req.cookie.jwt;
-//   console.log(req.cookie.jwt)
-//   if (!authorization || !authorization.startsWith('Bearer ')) {
+//   const { authorization } = req.headers;
+//   console.log(authorization)
+//   console.log(req.cookies)
+//   if (!authorization || !authorization.startsWith('jwt= ')) {
 //     throw new EmailOrPasswordError('Необходимо авторизоваться');
 //   }
 //
@@ -19,20 +39,3 @@ const EmailOrPasswordError = require('../errors/EmailOfPasswordError');
 //
 //   next();
 // };
-module.exports.auth = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new EmailOrPasswordError('Необходимо авторизоваться');
-  }
-
-  const token = authorization.replace('Bearer ', '');
-  let payload;
-  try {
-    payload = checkToken(token);
-  } catch (err) {
-    next(new EmailOrPasswordError('Необходимо авторизоваться'));
-  }
-  req.user = payload;
-
-  next();
-};
